2587b82ee53c266b7d2c361b8b02fcb0892a7114
d3nnyyy <d3nnn41k@gmail.com>
2023-06-24 14:20:41 +0300

    Initial commit

---
+++ src/main/java/ua/dtsebulia/spring/BookstoreManagementSystem/BookstoreManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package ua.dtsebulia.spring.BookstoreManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/dtsebulia/spring/BookstoreManagementSystem/BookstoreManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.dtsebulia.spring.BookstoreManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
