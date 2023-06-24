c94de87e63cc5b6bb7fd249aaa31d68eaa10bc01
d3nnyyy <d3nnn41k@gmail.com>
2023-06-24 15:49:57 +0300

    set all required configuration

---
+++ src/main/java/ua/dtsebulia/spring/BookstoreManagementSystem/config/DatasourceConfig.java
@@ -0,0 +1,30 @@
+package ua.dtsebulia.spring.BookstoreManagementSystem.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+public class DatasourceConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("app.datasource.main")
+    public HikariDataSource hikariDataSource() {
+        return DataSourceBuilder
+                .create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(HikariDataSource hikariDataSource) {
+        return new JdbcTemplate(hikariDataSource);
+    }
+
+
+}
